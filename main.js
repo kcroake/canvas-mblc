dbafe98c95c508f6324834e456af17bd3f32ffb2
kcroake <kcroake88@gmail.com>
2026-05-05 17:33:40 -0400

    bunch of changes.

--- main.js
+++ main.js
@@ -200,89 +200,256 @@ stage.add(furniturelayer);
 
 const centerx = stage.width() / 4;
 const centery = stage.height() / 4;
-
 const floorplan = new Konva.Rect({
     fill: '#acc9f0c1',
     shadowColor: 'rgba(0,0,0,0.15)', 
     shadowBlur: 10, 
     shadowOffsetY: 4,
 });
-
-
-
+const floorPlanLabel = new Konva.Text();
+const group = new Konva.Group();
 const chairXY = (pixels * 5);
+const shelfX = (pixels * 10);
+const shelfY = (pixels * 3);
 const padding = 20;
 let lastAddedElementXY = {x: 0, y: 0};
 let form = document.querySelector('form');
-let addChairButton = document.getElementById('add-chair');
+const addChairButton = document.getElementById('add-chair');
+const addShelfButton = document.getElementById('add-shelf');
+const deleteNode = document.getElementById('delete-node');
 let initialScale = 1; 
-const floorPlanLabel = new Konva.Text();
+group.add(floorplan);
+group.add(floorPlanLabel);
+floorlayer.add(group);
+let w, h, inputX = 1, inputY = 1;
+form.addEventListener('change', function(e){
+  let value = Number(e.target.value);
+  if("X" == e.target.name) {
+    inputX = value ? value : 1;
+    w = pixels * value;
+  }
+  if("Y" == e.target.name) {
+    inputY = value ? value : 1;
+    h = pixels * value;
+  }
+  let sqrtFootLabel
+  if(inputX != 1 && inputY != 1) {
+   sqrtFootLabel = (inputX * inputY);
+  }
+  floorPlanLabel.text(sqrtFootLabel);
+  floorplan.width(w);
+  floorplan.height(h);
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const data = new FormData(form);
-    const w = pixels * data.get('X');
-    const h = pixels * data.get('Y');
-    const sqrtFootLabel = (data.get('X') * data.get('Y')) + "sqft";
-    const group = new Konva.Group();
-    let centerpointy, centerpointx;
-    if((w/2) > centerx){
-      centerpointx = (w/2) - centerx;
-    } else {
-      centerpointx = centerx - (w/2);
-    }
-
-    if((h/2) > centery) {
-      centerpointy = (h/2) - centery
-    } else {
-      centerpointy = centery - (h/2);
-    }
-    
-    floorPlanLabel.text(sqrtFootLabel);
-
-    floorplan.width(w);
-    floorplan.height(h);
-   
-    lastAddedElementXY.x = w;  
-    
-    
-    
-    if(w > h) {
-        initialScale = 1024/w * 100
-    } else {
-        initialScale = 1024/h * 100
-    }
-
-    group.add(floorplan);
-    group.add(floorPlanLabel);
-    floorlayer.add(group);
 });
 
-addChairButton.addEventListener('click', function(){
-    console.log(lastAddedElementXY);
-    const group = new Konva.Group({
-      draggable: true
-    });
-    const chairLabel = new Konva.Text({
-      text: 'Chair 5x5ft',
-      x: lastAddedElementXY.x,
-    });
-    const chair = new Konva.Rect({ 
-        width: chairXY,
-        height: chairXY,
-        x: lastAddedElementXY.x,
-        fill: '#1b9c80',
-        //draggable: true,
-        shadowColor: 'rgba(0,0,0,0.15)', 
-        shadowBlur: 10, 
-        shadowOffsetY: 4,
-    });    
-    group.add(chair);
-    group.add(chairLabel);
-    furniturelayer.add(group);
-}); 
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+});
 
 
+const chairGroup = new Konva.Group({
+  draggable: true,
+  name: 'furniture',
+  visible: false
+});
+
+const chairLabel = new Konva.Text({
+  text: 'Chair 5x5',
+});
+
+const chair = new Konva.Rect({ 
+    width: chairXY,
+    height: chairXY,
+    fill: '#1b9c80',
+    //draggable: true,
+    shadowColor: 'rgba(0,0,0,0.15)', 
+    shadowBlur: 10, 
+    shadowOffsetY: 4,
+    
+});    
+
+chairGroup.add(chair);
+chairGroup.add(chairLabel);
+furniturelayer.add(chairGroup);
+
+
+const shelfGroup = new Konva.Group({
+  draggable: true,
+  name: 'furniture',
+  visible: false
+});
+
+const selfLabel = new Konva.Text({
+  text: 'Shelf 10x3',
+});
+
+const shelf = new Konva.Rect({ 
+    width: shelfX,
+    height: shelfY,
+    fill: '#1b9c80',
+    //draggable: true,
+    shadowColor: 'rgba(0,0,0,0.15)', 
+    shadowBlur: 10, 
+    shadowOffsetY: 4,
+    
+});    
+
+shelfGroup.add(shelf);
+shelfGroup.add(selfLabel);
+furniturelayer.add(shelfGroup);
+
+const tr = new Konva.Transformer({
+  rotateEnabled: true,
+  resizeEnabled: false,
+  borderEnabled: true,
+});
+
+furniturelayer.add(tr);
+
+let selectionRectangle = new Konva.Rect({
+  fill: 'rgba(0,0,255,0.5)',
+  visible: false,
+});
+
+furniturelayer.add(selectionRectangle);
+
+let chairI = 0;
+let shelfI = 0;
+let newChair;
+addChairButton.addEventListener('click', function(e){
+  e.preventDefault();
+  newChair = chairGroup.clone({
+    visible: true,
+  })
+  furniturelayer.add(newChair);
+  tr.nodes([newChair]);
+  furniturelayer.batchDraw();
+  chairI++;
+});
+
+
+chairGroup.on('click', ()=> {
+  console.log('clicked');
+  tr.node(); //clear group
+  tr.node([chairGroup]);
+})
+
+
+addShelfButton.addEventListener('click', function(e){
+  e.preventDefault();
+  const newShelf = shelfGroup.clone({
+    visible: true
+  });
+  furniturelayer.add(newShelf);
+  tr.nodes([newShelf]);
+  furniturelayer.batchDraw();
+  shelfI++;
+});
+
+// furniture.on('click', function(){
+//   console.log(this);
+  
+// });
+
+
+deleteNode.addEventListener('click',function(e){
+  e.preventDefault();
+});
+
+
+let x1, y1, x2, y2;
+stage.on('mousedown touchstart', (e) => {
+  // do nothing if we mousedown on any shape
+  if (e.target !== stage) {
+    return;
+  }
+  x1 = stage.getPointerPosition().x;
+  y1 = stage.getPointerPosition().y;
+  x2 = stage.getPointerPosition().x;
+  y2 = stage.getPointerPosition().y;
+
+  selectionRectangle.setAttrs({
+    x: x1,
+    y: y1,
+    width: 0,
+    height: 0,
+    visible: true,
+  });
+});
+
+stage.on('mousemove touchmove', () => {
+  // do nothing if we didn't start selection
+  if (!selectionRectangle.visible()) {
+    return;
+  }
+  x2 = stage.getPointerPosition().x;
+  y2 = stage.getPointerPosition().y;
+
+  selectionRectangle.setAttrs({
+    x: Math.min(x1, x2),
+    y: Math.min(y1, y2),
+    width: Math.abs(x2 - x1),
+    height: Math.abs(y2 - y1),
+  });
+});
+
+stage.on('mouseup touchend', () => {
+  // do nothing if we didn't start selection
+  if (!selectionRectangle.visible()) {
+    return;
+  }
+  // update visibility in timeout, so we can check it in click event
+  setTimeout(() => {
+    selectionRectangle.visible(false);
+  });
+
+  var shapes = stage.find('.furniture');
+  var box = selectionRectangle.getClientRect();
+  var selected = shapes.filter((shape) =>
+    Konva.Util.haveIntersection(box, shape.getClientRect())
+  );
+  tr.nodes(selected);
+});
+
+// clicks should select/deselect shapes
+stage.on('click tap', function (e) {
+// if we are selecting with rect, do nothing
+if (selectionRectangle.visible() && selectionRectangle.width() > 0 && selectionRectangle.height() > 0) {
+  return;
+}
+
+  // if click on empty area - remove all selections
+  if (e.target === stage) {
+    tr.nodes([]);
+    return;
+  }
+
+  // do nothing if clicked NOT on our rectangles
+  if (!e.target.hasName('furniture')) {
+    return;
+  }
+
+  // do we pressed shift or ctrl?
+  const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+  const isSelected = tr.nodes().indexOf(e.target) >= 0;
+
+  if (!metaPressed && !isSelected) {
+    // if no key pressed and the node is not selected
+    // select just one
+    tr.nodes([e.target]);
+  } else if (metaPressed && isSelected) {
+    // if we pressed keys and node was selected
+    // we need to remove it from selection:
+    const nodes = tr.nodes().slice(); // use slice to have new copy of array
+    // remove node from array
+    nodes.splice(nodes.indexOf(e.target), 1);
+    tr.nodes(nodes);
+  } else if (metaPressed && !isSelected) {
+    // add the node into selection
+    const nodes = tr.nodes().concat([e.target]);
+    tr.nodes(nodes);
+  }
+});
 
 
  /**
